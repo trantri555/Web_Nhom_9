b8abf38072a2d79215d700d63f8541e36f2095ae
sang <23130270@st.hcmuaf.edu.vn>
2025-12-20 14:58:16 +0700

    chức năng Quản lý Sản phẩm (Product)-productmodel,productDAO, productServlet

---
+++ src/main/java/model/User.java
@@ -0,0 +1,12 @@
+public class User {
+    private int id;
+    private String name;
+    private String email;
+
+    // Constructor, Getter, Setter
+    public User() {}
+    public User(int id, String name, String email) {
+        this.id = id; this.name = name; this.email = email;
+    }
+    // ... Thêm các Getter và Setter ở đây
+}

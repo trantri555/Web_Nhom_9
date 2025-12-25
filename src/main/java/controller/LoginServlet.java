407eddcbe31bcc751fa8af324f11a96da10ebcd3
Tran Duc Tri <23130347@st.hcmuaf.edu.vn>
2025-12-25 21:07:17 +0700

    fix id, type trong login

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -10,11 +10,17 @@ import java.io.IOException;
 @WebServlet(name = "LoginServlet", value = "/login")
 public class LoginServlet extends HttpServlet {
     @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("login.jsp").forward(request, response);
+    }
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         // 1. Lấy dữ liệu từ JSP
-        String user = request.getParameter("username");
+        String user = request.getParameter("loginEmail");
         String pass = request.getParameter("password");
+
+
 
         // 2. Gọi lớp DAO để kiểm tra
         UserDAO dao = new UserDAO();
@@ -24,7 +30,7 @@ public class LoginServlet extends HttpServlet {
             // Đăng nhập thành công: Lưu vào Session và chuyển đến trang chủ
             HttpSession session = request.getSession();
             session.setAttribute("acc", u);
-            response.sendRedirect("home.jsp");
+            response.sendRedirect("index.jsp");
         } else {
             // Đăng nhập thất bại: Báo lỗi và quay lại trang login
             request.setAttribute("mess", "Sai tài khoản hoặc mật khẩu!");
